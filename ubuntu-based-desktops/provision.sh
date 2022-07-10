b6674b72886f74c59a83fce1f3304d10df779c2c
kwnaidoo <kwnaidoo@gmail.com>
2022-07-10 15:19:06 +0200

    Update provision.sh

--- ubuntu-based-desktops/provision.sh
+++ ubuntu-based-desktops/provision.sh
@@ -78,13 +78,14 @@ sudo mysql -e "create user $USER@localhost identified by '1234';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO $USER@localhost;"
 sudo mysql -e "FLUSH PRIVILEGES;"
 
-## cuda drivers -- install nvida drivers first
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pinsudo 
-mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.debsudo 
+## cuda drivers ubuntu 20.04 -- install nvida drivers first
+cd /tmp/ && wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv /tmp/cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+cd /tmp/ && wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
+sudo dpkg -i /tmp/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.debsudo 
 cp /var/cuda-repo-ubuntu2004-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/sudo 
-apt-get updatesudo apt-get -y install cuda
+apt-get update
+sudo apt-get -y install cuda
 
 ## Prettify default terminal ##
 
